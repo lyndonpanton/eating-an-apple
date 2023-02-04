4391579ea56cc389914e911c2f4533339e97e752
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-04 21:56:43 +0000

    Implemented apple class and its TakeBite method

--- Exercise6/Exercise6/Program.cs
+++ Exercise6/Exercise6/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Exercise6
+﻿using Exercise6;
+
+namespace Exercise5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Apple apple = new Apple(true, 3.2f);
+
+            Console.WriteLine("My apple is organic: " + apple.Organic);
+            Console.WriteLine("The remaining size of my apple is: " + apple.AmountLeft);
+            Console.WriteLine();
+
+            float bite = 0.3f;
+
+            while (apple.AmountLeft > 0)
+            {
+                apple.TakeBite(bite);
+                Console.WriteLine("Remaining size of my apple: " + apple.AmountLeft);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Apple is now finished");
         }
     }
 }
